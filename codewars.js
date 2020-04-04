ef53142154e2421a06f97026f14ff9372df86c62
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-03 23:29:37 -0700

    solution to adding values of an array excluding the highest and lowest value

--- codewars.js
+++ codewars.js
@@ -5,4 +5,12 @@ const squareDigits= (num) =>parseInt(num.toString().split('').map((number)=>Math
 
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 const removeChar =(str) => str.slice(1,-1)
-console.log(removeChar('abcd'));
+
+// Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
+// (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
+// Example:
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// If array is empty, null or None, or if only 1 Element exists, return 0.
+// Note:In C++ instead null an empty vector is used. In C there is no null. ;-)
+const sumArray = (array)=> array.filter((value)=> value< Math.max(...array) && value > Math.min(...array)).reduce((a,b)=> a+b,0);
