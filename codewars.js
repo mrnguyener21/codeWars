16b58dee46e5c3d204b1425464d71c7a96d3f062
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-03 22:21:59 -0700

    solution to removing first and last characters of a string

--- codewars.js
+++ codewars.js
@@ -3,3 +3,6 @@
 // Note: The function accepts an integer and returns an integer
 const squareDigits= (num) =>parseInt(num.toString().split('').map((number)=>Math.pow(number,2)).join(''))
 
+// It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+const removeChar =(str) => str.slice(1,-1)
+console.log(removeChar('abcd'));
