ae2c5f144b0d3792fb42848dff90ca3baa080848
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-04 17:23:30 -0700

    solution for finding errrors in printer label

--- codewars.js
+++ codewars.js
@@ -4,8 +4,10 @@
 // Note: The function accepts an integer and returns an integer
 const squareDigits = (num) => parseInt(num.toString().split('').map((number) => Math.pow(number, 2)).join(''));
 
+
 // It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
 const removeChar = (str) => str.slice(1, -1);
+
 
 // Sum all the numbers of the array (in F# and Haskell you get a list) except the highest and the lowest element (the value, not the index!).
 // (The highest/lowest element is respectively only one element at each edge, even if there are more than one with the same value!)
@@ -26,6 +28,7 @@ const sumArray = (array) => {
   return array.sort((a, b) => a - b).slice(1, -1).reduce((a, b) => a + b, 0);
 }
 
+
 // Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
 // For example,
 // [true,  true,  true,  false,
@@ -40,9 +43,27 @@ const sumArray = (array) => {
 //true = present, false null undefined etc = not present
 //check each index of an array for value of true
 
+// const countSheeps = (arrayOfSheeps) => arrayOfSheeps.filter(Boolean).length; better solution created by another user
 const countSheeps = (arrayOfSheep) => {
   let counter = 0;
   arrayOfSheep.map((sheep) => {sheep === true? counter ++:counter+0})
   return counter;
 }
-const countSheeps = (arrayOfSheeps) => arrayOfSheeps.filter(Boolean).length;//better solution created by another user
+
+
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+// You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+// The string has a length greater or equal to one and contains only letters from ato z.
+// #Examples:
+// s="aaabbbbhaijjjm"
+// error_printer(s) => "0/14"
+// s="aaaxbbbbyyhwawiwjjjwwm"
+// error_printer(s) => "8/22"
+const printerError = (s) => {
+  const correctArray = ['a','b','c','d','e','f','g','h','i','j','k','l','m']
+  let error = 0;
+  s.split('').map((letter) => {correctArray.includes(letter)? error +0:error ++})
+  return `${error}/${s.length}`
+}
