0774ea3b0e678e8465c8685e42b77687cdf7d225
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-08 20:07:21 -0700

    solved challenge to remove string from array

--- codewars.js
+++ codewars.js
@@ -193,4 +193,11 @@ const minMax =(arr) => [Math.min(...arr),Math.max(...arr)]
 // Try to do it without using if statements!
 const arithmetic = (a,b, operator) => operator === 'subtract'? a-b:operator === 'add'?a+b:operator === 'multiply'? a*b:a/b;
 
-console.log(arithmetic(1,2,'something'))
+
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+const filter_list=(l) => l.filter((x)=> typeof x === 'number') 
