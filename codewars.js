6274dcd6929ba9317590e0e9284d42207dd73127
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-15 16:46:08 -0700

    solved challenge to round number to next multiple of 5

--- codewars.js
+++ codewars.js
@@ -276,3 +276,25 @@ const minValue = (values) => parseInt(values.filter((a,b) =>  values.indexOf(a) 
 
 minValue([4,7,5,7])
 // console.log(minValue([4,7,5,7]))
+
+// Given an integer as input, can you round it to the next (meaning, "higher") 5?
+
+// Examples:
+
+// input:    output:
+// 0    ->   0
+// 2    ->   5
+// 3    ->   5
+// 12   ->   15
+// 21   ->   25
+// 30   ->   30
+// -2   ->   0
+// -5   ->   -5
+// etc.
+// Input may be any positive or negative integer (including 0).
+
+// You can assume that all inputs are valid integers.
+const roundToNext5 = (n) => Math.ceil(n/5)*5;
+
+
+
