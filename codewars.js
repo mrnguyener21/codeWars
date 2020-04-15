c96125a14d094a4dde58f8ef629c9ca1619e48dc
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-14 18:27:52 -0700

    solved challenge to combine array into a single value sorted and without duplicates

--- codewars.js
+++ codewars.js
@@ -250,9 +250,29 @@ const highAndLow = (numbers) => {
   }
 }
 // highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6")
-console.log(highAndLow("42"))
+// console.log(highAndLow("42"))
 // BELOW IS THE BEST PRACTICE
 // function highAndLow(numbers){
 //   numbers = numbers.split(' ').map(Number);
 //   return Math.max.apply(0, numbers) + ' ' + Math.min.apply(0, numbers);
 // }
+
+
+// Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+// Notes:
+// Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+// Input >> Output Examples
+// minValue ({1, 3, 1})  ==> return (13)
+// Explanation:
+// (13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+// minValue({5, 7, 5, 9, 7})  ==> return (579)
+// Explanation:
+// (579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+// minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+// Explanation:
+// (134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications
+
+const minValue = (values) => parseInt(values.filter((a,b) =>  values.indexOf(a) === b).sort().join(''))
+
+minValue([4,7,5,7])
+// console.log(minValue([4,7,5,7]))
