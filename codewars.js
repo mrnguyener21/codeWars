0e051cb9985310b903863d276d3e5a06603b60fd
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-18 04:46:20 -0700

    failed challenge of returning divisble

--- codewars.js
+++ codewars.js
@@ -348,4 +348,27 @@ rowSumOddNumbers(4)
 const isAnagram = (test,original) => test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('') ? true : false ;
 
 isAnagram("Twoo", "WooT");
-console.log(isAnagram("Twoo", "WooT"))
+
+
+// Count the number of divisors of a positive integer n.
+
+// Random tests go up to n = 500000.
+
+// Examples
+// divisors(4)  = 3  // 1, 2, 4
+// divisors(5)  = 2  // 1, 5
+// divisors(12) = 6  // 1, 2, 3, 4, 6, 12
+// divisors(30) = 8  // 1, 2, 3, 5, 6, 10, 15, 30
+
+
+
+
+function getDivisorsCnt(n) {
+  for (var divisble = 0, i = n; i > 0; i--) {
+    if (n % i == 0) divisble++;
+  }
+  
+  return divisble;
+}
+getDivisorsCnt(54);
+
