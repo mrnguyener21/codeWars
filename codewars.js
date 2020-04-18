f72a96291e1c419fe07d532bf709c37c37fd17ab
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-17 19:30:12 -0700

    solved anagram challenge

--- codewars.js
+++ codewars.js
@@ -322,3 +322,30 @@ const roundToNext5 = (n) => Math.ceil(n/5)*5;
 const rowSumOddNumbers = (n) => Math.pow(n,3)
 rowSumOddNumbers(4)
 
+
+
+
+
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+
+// Note: anagrams are case insensitive
+
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+// Examples
+// "foefet" is an anagram of "toffee"
+
+// "Buckethead" is an anagram of "DeathCubeK"
+
+// const isAnagram = (test, original) => {
+// const testWord = test.toLowerCase().split('').sort().join('')
+// console.log(testWord)
+// const originalWord = original.toLowerCase().split('').sort().join('');
+// console.log(originalWord)
+// return testWord === originalWord? true : false;
+// }
+
+const isAnagram = (test,original) => test.toLowerCase().split('').sort().join('') === original.toLowerCase().split('').sort().join('') ? true : false ;
+
+isAnagram("Twoo", "WooT");
+console.log(isAnagram("Twoo", "WooT"))
