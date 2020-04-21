20118927173adde1c7ba5dfa03fe8628d49becc1
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-20 18:24:59 -0700

    failed factorial challenge

--- codewars.js
+++ codewars.js
@@ -397,3 +397,20 @@ getDivisorsCnt(54);
 
 const isSquare = (n) => Number.isInteger(Math.sqrt(n))
 isSquare(25);
+
+
+// In mathematics, the factorial of a non-negative integer n, denoted by n!, is the product of all positive integers less than or equal to n. For example: 5! = 5 * 4 * 3 * 2 * 1 = 120. By convention the value of 0! is 1.
+
+// Write a function to calculate factorial for a given input. If input is below 0 or above 12 throw an exception of type ArgumentOutOfRangeException (C#) or IllegalArgumentException (Java) or RangeException (PHP) or throw a RangeError (JavaScript) or ValueError (Python) or return -1 (C).
+
+const factorial = (n) => {
+if(n < 0){
+  return -1;
+}else if(n === 0){
+  return 1
+}else {
+  return (factorial(n-1)*n)
+}
+}
+// factorial(0);
+console.log(factorial(1));
