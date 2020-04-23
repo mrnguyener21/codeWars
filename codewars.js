7ff297a910a76221edfabb98b05543a5fdfffd4d
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-22 17:15:10 -0700

    solved challenge, determine if sum of array is even or odd

--- codewars.js
+++ codewars.js
@@ -448,5 +448,23 @@ const maxMultiple = (divisor, bound) => {
   return Math.max(...list)
 }
 // bound - bound%divisor was the best choice
+
 maxMultiple(2,7);
 
+
+// Task:
+// Given a list of numbers, determine whether the sum of its elements is odd or even.
+// Give your answer as a string matching "odd" or "even".
+// If the input array is empty consider it as: [0] (array with a zero).
+// Example:
+// odd_or_even([0])          ==  "even"
+// odd_or_even([0, 1, 4])    ==  "odd"
+// odd_or_even([0, -1, -5])  ==  "even"
+
+// console.log(array.reduce((currentNumber, numberBeingAdded) => currentNumber+numberBeingAdded)%2)
+const oddOrEven = (array) => array.reduce((currentNumber, numberBeingAdded) => currentNumber+numberBeingAdded,0) % 2 === 0? 'even':'odd';
+
+  
+
+oddOrEven([0,1,5]);
+console.log(oddOrEven([-1023,1,-2]))
