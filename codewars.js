0dbd67db43a14ba3ccade53c838f56c9911479e7
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-04-23 17:22:43 -0700

    solved challenge of adding number with any numbers leading up to it that is divisble by 3 or 5

--- codewars.js
+++ codewars.js
@@ -463,8 +463,37 @@ maxMultiple(2,7);
 
 // console.log(array.reduce((currentNumber, numberBeingAdded) => currentNumber+numberBeingAdded)%2)
 const oddOrEven = (array) => array.reduce((currentNumber, numberBeingAdded) => currentNumber+numberBeingAdded,0) % 2 === 0? 'even':'odd';
-
-  
-
 oddOrEven([0,1,5]);
-console.log(oddOrEven([-1023,1,-2]))
+
+
+
+// Your task is to write function findSum.
+
+// Upto and including n, this function will return the sum of all multiples of 3 and 5.
+
+// For example:
+
+// findSum(5) should return 8 (3 + 5)
+
+// findSum(10) should return 33 (3 + 5 + 6 + 9 + 10)
+
+const findSum = (number) => {
+  let divisibleNumbers = [];
+
+  for (let i = 0;  i< number; i++) {
+    if( i%3 === 0){
+      divisibleNumbers.push(i);
+    } else if ( i%5 === 0){
+      divisibleNumbers.push(i)
+    }else{
+      null;
+    }
+  }
+
+  return number + (divisibleNumbers.reduce((a,b) => a+b, 0));
+};
+findSum(10);
+
+
+
+
