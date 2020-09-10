d574ea89c187d6c48791a34108c62f4b807dc4bd
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-09-10 08:30:35 -0700

    failed challenge

--- codewars.js
+++ codewars.js
@@ -759,20 +759,51 @@ capitalize("abcdef")
 
 // The middle character(s) of the word represented as a string.
 
-const getMiddle =(s) => {
+// const getMiddle =(s) => {
 // check the length to determine if it is the length is even or odd. 
-const check = s.length;
-console.log(check)
+// const check = s.length;
+// console.log(check)
 
-if (check%2 === 0){
-  console.log('even')
-}else{
-  console.log('odd');
-}
+// if (check%2 === 0){
+// }else{
+// }
 //if odd
   //
 //if even
   //
-}
-getMiddle('testy');
+// }
+// getMiddle('testy');
 // console.log(getMiddle('test'));
+
+
+
+
+
+// Create a function close_compare that accepts 3 parameters: a, b, and an optional margin. The function should return whether a is lower than, close to, or higher than b. a is "close to" b if margin is higher than or equal to the difference between a and b.
+
+// When a is lower than b, return -1.
+
+// When a is higher than b, return 1.
+
+// When a is close to b, return 0.
+
+// If margin is not given, treat it as zero.
+
+// Example: if a = 3, b = 5 and the margin = 3, since a and b are no more than 3 apart, close_compare should return 0. Otherwise, if instead margin = 0, a is lower than b and close_compare should return -1.
+
+// Assume: margin >= 0
+
+// Tip: Some languages have a way to make arguments optional.
+
+const closeCompare = (a, b, margin) =>{
+
+  if(a < b){
+    Math.abs(a-b) <= margin? 0 : -1 
+  } else if ( a > b ){
+    Math.abs(a-b) <= margin? 0 : 1 
+  } else{
+    return 0
+  }
+}
+closeCompare(4,5)
+console.log(closeCompare(4,5))
