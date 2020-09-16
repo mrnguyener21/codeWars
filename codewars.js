4b814af201bf45792c43505d4fa4b8193a139a0e
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-09-15 23:04:21 -0700

    in process of solving hello, name or world coding challenge

--- codewars.js
+++ codewars.js
@@ -831,6 +831,23 @@ capitalize("abcdef")
 
 // If you make it, return "Alive!", if not, return "Shark Bait!".
 
-const shark = (pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) => {
+// const shark = (pontoonDistance, sharkDistance, youSpeed, sharkSpeed, dolphin) => {
 
+// }
+
+
+//hello, name or world!
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+// Examples:
+
+// hello "john"   => "Hello, John!"
+// hello "aliCE"  => "Hello, Alice!"
+// hello          => "Hello, World!" # name not given
+// hello ''       => "Hello, World!" # name is an empty String
+
+const hello = (name) => {
+  return '';
 }
