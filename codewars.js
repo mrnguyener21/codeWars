2381c928f47a2c232c1b583c7a457873af536553
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-09-18 07:33:03 -0700

    solved The falling speed of petals

--- codewars.js
+++ codewars.js
@@ -872,6 +872,38 @@ capitalize("abcdef")
 //Multiply
 //This code does not execute properly. Try to figure out why
 
-const multiply = (a, b) => a*b
-multiply()
-console.log(multiply(2,1))
+// const multiply = (a, b) => a*b
+// multiply()
+// console.log(multiply(2,1))
+
+//FIlling an array (part 1)
+
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+// arr(5) // => [0,1,2,3,4]
+
+
+// The falling speed of petals
+// When it's spring Japanese cherries blossom, it's called "sakura" and it's admired a lot. The petals start to fall in late April.
+
+// Suppose that the falling speed of a petal is 5 centimeters per second (5 cm/s), and it takes 80 seconds for the petal to reach the ground from a certain branch.
+
+// Write a function that receives the speed (in cm/s) of a petal as input, and returns the time it takes for that petal to reach the ground from the same branch.
+
+// Notes:
+
+// The movement of the petal is quite complicated, so in this case we can see the velocity as a constant during its falling.
+// Pay attention to the data types.
+// If the initial velocity is non-positive, the return value should be 0
+
+
+// falling speed = 5 cm/second => 80 seconds to the ground
+// 80 secondc to reach the groun
+// create a function to  determine how fast based on velocity
+//from the branch to the ground it is 16 cm
+const sakuraFall = (fallingSpeed) => fallingSpeed > 0 ? 400/fallingSpeed : 0;
+console.log(sakuraFall(10))
