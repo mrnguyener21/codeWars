3051a202e6c3049bbc687879caed96a7c0894a10
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-09-28 08:17:44 -0700

    attempting coding challenge number in stings

--- codewars.js
+++ codewars.js
@@ -996,3 +996,13 @@ const accum = (s) => {
 accum('abcd')
 
 
+//NUmbers in strings
+// In this Kata, you will be given a string that has lowercase letters and numbers. Your task is to compare the number groupings and return the largest number.
+
+// For example, solve("gh12cdy695m1") = 695, because this is the largest of all number groupings.
+
+// Good luck!
+const solve = (s) => {
+
+  };
+solve('gh12cdy695m1');
