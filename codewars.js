44c36d03f37560e58d410c9085690b2aabc3c15d
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-07 15:45:51 -0700

    attempting coding challenge Credit Card Mask

--- codewars.js
+++ codewars.js
@@ -1264,19 +1264,33 @@ const nbYear = (p0, percent, aug, p) => {
 // based on the position, it repeats itself three times
 //the first letter of each sequence will always be capital
 
-
-//my answer
-// const accum = (s) => {
-//   const answer = [];
-//   s.toLowerCase().split('').map((letter, i) => {
-//     const formula = letter.toUpperCase().concat(letter.repeat(i))
-//     answer.push(formula)
-//   })
-//   return answer.join('-');
-// }
-
 const accum = (s) => s.toLowerCase().split('').map((letter, i) => letter.toUpperCase().concat(letter.repeat(i))).join('-')
-
 
 // accum('ZpglnRxqenU')
 // console.log(accum('ZpglnRxqenU'))
+
+
+
+
+
+
+//Credit Card Mask
+// Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+
+// Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+// Examples
+// maskify("4556364607935616") == "############5616"
+// maskify(     "64607935616") ==      "#######5616"
+// maskify(               "1") ==                "1"
+// maskify(                "") ==                 ""
+
+// // "What was the name of your first pet?"
+// maskify("Skippy")                                   == "##ippy"
+// maskify("Nananananananananananananananana Batman!") == "####################################man!"
+
+const maskify = (cc) => {
+
+}
+
+maskify('4556364607935616')
