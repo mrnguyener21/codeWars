f88bb342f0b9b64b0b3813ffebac786e17709b42
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-07 10:43:38 -0700

    reattempting coding challenge mumble

--- codewars.js
+++ codewars.js
@@ -1247,3 +1247,19 @@ const nbYear = (p0, percent, aug, p) => {
   return counter;
 }
 // console.log(nbYear(1500, 5, 100, 5000))
+
+
+
+// Mumbling
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+const accum = (s) => {
+	// your code
+}
