7100d9e4ee2ce3d4996829f4239ddc81ff2f2fd3
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-06 21:37:48 -0700

    working on coding challenge Beginnner Series #3 Sum of Numbers

--- codewars.js
+++ codewars.js
@@ -1145,3 +1145,32 @@ const XO = (str) => str.toLowerCase().split('x').length === str.toLowerCase().sp
 const toJadenCase = (str) => str.split(' ').map((word) => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
 // toJadenCase("How can mirrors be real if our eyes aren't real");
 // console.log(toJadenCase("How can mirrors be real if our eyes aren't real"))
+
+
+
+//Beginner Series #3 Sum of Numbers
+
+// Given two integers a and b, which can be positive or negative, find the sum of all the numbers between including them too and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// Examples
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+const getSum = (a,b) => {
+  if ( a === b){
+    return a;
+  }else if(a < b ) {
+    
+  } else if (a > b){
+
+  }
+
+  }
+
+console.log(getSum(2,1))
