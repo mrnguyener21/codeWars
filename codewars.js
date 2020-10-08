759125b60f87fc5a957e5f7a78c627752ea13e76
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-08 05:49:26 -0700

    solved coding challenge Who Likes it

--- codewars.js
+++ codewars.js
@@ -1336,7 +1336,7 @@ const validatePIN = (pin) => {
     return false;
   }
 }
-console.log(validatePIN('09876 '))
+// console.log(validatePIN('09876 '))
 
 
 
@@ -1352,6 +1352,23 @@ console.log(validatePIN('09876 '))
 // likes ["Max", "John", "Mark"] -- must be "Max, John and Mark like this"
 // likes ["Alex", "Jacob", "Mark", "Max"] -- must be "Alex, Jacob and 2 others like this"
 
+// empty array = 'no one likes this'
+// one object in the array = ' `object` likes this'
+//two objects inthe array '`object 1` and `object 2` like this'
+
+//! in this case using a switch statement would've alot cleaner and it would've saved a little bit of time processsing
+
 const likes = (names) => {
-  
+  if (names.length === 1) {
+    return `${names[0]} likes this`;
+  } else if (names.length === 2) {
+    return `${names[0]} and ${names[1]} like this`;
+  } else if (names.length === 3) {
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  } else if (names.length >= 4){
+    return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`;
+  } else{
+    return 'no one likes this';
+  }
 }
+// console.log(likes([]))
