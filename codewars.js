e6a15f89cc045e1a88a7772c6271ece272acfb61
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-07 17:23:54 -0700

    attempting coding challenge Regex validate PIN code

--- codewars.js
+++ codewars.js
@@ -1313,3 +1313,21 @@ const maskify = (creditCardNumber) => {
 
 maskify('4556364607935616')
 // console.log(maskify('4556364607935616'))
+
+
+
+// Regex Validate PIN Code
+
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+const validatePIN = (pin) => {
+
+}
+pin('1234')
