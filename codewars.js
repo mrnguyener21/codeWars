727c70015fb4554efe3c6a97e3dfbfc1dd386a61
mrnguyener21 <victortnguyen.workemail@gmail.com>
2020-10-14 21:35:10 -0700

    coding challenge password should not contain any part of your username

--- codewars.js
+++ codewars.js
@@ -1474,7 +1474,7 @@ const sumTwoSmallestNumbers = (numbers) => {
 // You are writing a password validator for a website. You want to discourage users from using their username as part of their password, or vice-versa, because it is insecure. Since it is unreasonable to simply not allow them to have any letters in common, you come up with this rule:
 
 // For any password and username pair, the length of the longest common substring should be less than half the length of the shortest of the two.
-
+  
 // In other words, you won't allow users to have half their password repeated in their username, or half their username repeated in their password.
 
 // Write a function validate(username, password) which returns true if your rule is followed, false otherwise.
@@ -1484,8 +1484,9 @@ const sumTwoSmallestNumbers = (numbers) => {
 // Both the username and the password may contain uppercase letters, lowercase letters, numbers, spaces, and the following special/punctation characters: !@#$%^&*()_+{}:"<>?[];',.
 // The username and password will each be less than 100 characters.
 
-
+//NEED TO LEARN MORE ABOUT SUBSTRINGS AND THEIR METHODS
 
 const validate = (username, password) => {
   
 }
+validate("sword", "password" );
